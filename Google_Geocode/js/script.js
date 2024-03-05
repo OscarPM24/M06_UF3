47100d5dfda013ed8c08d8279f0ad8957e512020
Oscar Pérez <2023_oscar.perez@iticbcn.cat>
2024-03-05 17:11:30 +0100

    practica terminada amb comentaris

--- Google_Geocode/js/script.js
+++ Google_Geocode/js/script.js
@@ -1,36 +1,65 @@
 let geocoder = new google.maps.Geocoder(); // Variable del Geocoder de Google
 let findLoc = document.getElementById("findLoc"); // Botón para buscar una dirección
-let backCenter = document.getElementById("backCenter"); // Botón para volver al centro
-let myLatLng = new google.maps.LatLng(41.390205, 2.154007);
-let map; 
+let ubicacion = document.getElementById("ubicacion"); // Botón para volver al centro
+let myLatLng = new google.maps.LatLng(41.390205, 2.154007); // Latitud y longitud por defecto
+let map; // Mapa
 
 // Función que genera el mapa con la posición establecida y con un marcador
 async function initMap(pos, zoom) {
     const { Map } = await google.maps.importLibrary("maps");
     map = new Map(document.getElementById("map"), {
         center: pos, // coordenadas del centro del mapa
-        zoom: zoom, // zoom del maap
+        zoom: zoom, // zoom del map      
     });
 
+    createMarker(pos, map);
+}
+
+// Creamos el mapa
+initMap(myLatLng, 12);
+
+// EventListener del botón para que al clicarlo actualice el mapa con el nombre de la calle que hayamos puesto
+findLoc.addEventListener("click", geolocalitza);
+
+// EventListener que recoje tus coordenadas actuales y envia el mapa hacia esa posición, pone un marker y cambia el zoom
+ubicacion.addEventListener("click", function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            map.setCenter(pos);
+            map.setZoom(9);
+            
+            let marker = createMarker(pos, map, "./marker.png");
+        });
+    }
+});
+
+// Función que crea un marker con la posición, mapa y icono pasados por paràmetros, también añade una infoWindow
+function createMarker(pos, map, icon) {
     let marker = new google.maps.Marker({
-        position: map.center, // posición del marcador
+        position: pos, // posición del marcador
         map, // mapa
         title: "Barcelona", // nombre del marcador
+        icon: icon
     });
 
     let infowindow = new google.maps.InfoWindow({
-        content: "STRING + html"
+        content: document.getElementById("adreca").value
     });
     
     marker.addListener('click', function() {
         infowindow.open(map, marker);
     });
+
+    return marker;
 }
 
-initMap(myLatLng, 12);
-
-// EventListener del botón para que al clicarlo actualice el mapa con el nombre de la calle que hayamos puesto
-findLoc.addEventListener("click", function() {
+// Funcion que dada una dirección, centra el mapa y pone el marcador en ese lugar
+function geolocalitza() {
     let geocoder = new google.maps.Geocoder();
     let address = document.getElementById("adreca").value;
     geocoder.geocode( { 'address': address}, function(results, status) {
@@ -45,25 +74,4 @@ findLoc.addEventListener("click", function() {
             document.getElementById("longitude").value = longitude;
         } else alert("Adreça no trobada!");
     });
-});
-
-// EventListener que recoje tus coordenadas actuales y envia el mapa hacia esa posición, pone un marker y cambia el zoom
-backCenter.addEventListener("click", function() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            let pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            map.setCenter(pos);
-            map.setZoom(9);
-            
-            let marker = new google.maps.Marker({
-                position: pos,
-                map: map,
-                icon: "./marker.png"
-            });
-        });
-    }
-});
+}
